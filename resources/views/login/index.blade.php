bd11d3004ca252abe056ecf71cbff29092a1ea1a
Galih Agus Saputra <agussaputragalih@gmail.com>
2024-09-05 06:16:38 +0700

    Assets

---
+++ resources/views/login/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+@section('navhead')
+<div class="header header-fixed header-logo-center">
+    @include('layouts.navhead')
+</div>
+@endsection
+@section('content')
+
+@endsection
