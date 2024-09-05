fe56c61582e9ca6e3f99323eaafcc43c2be8bf7c
Galih Agus Saputra <agussaputragalih@gmail.com>
2024-09-05 11:12:24 +0700

    Page

---
+++ resources/views/order/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+@section('navhead')
+@section('title','Order')
+<div class="header header-fixed header-logo-center">
+    @include('layouts.navhead')
+
+</div>
+@endsection
+
+
+@section('content')
+
+@endsection
